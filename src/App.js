5f3022d873e4de939bf2b494191aba2726de3ddd
Maryam Khan <maryam.b.khan@edu.sait.ca>
2022-07-14 15:54:00 -0600

    added navbar

--- src/App.js
+++ src/App.js
@@ -6,16 +6,18 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Fun facts about React</h1>
-      <ul>
-        <li>Was first released in 2013</li>
-        <li>Was originally created by Jordan Walke</li>
-        <li>Has well over 100K stars on GitHub</li>
-        <li>Is maintained by Facebook</li>
-        <li>Powers thousands of enterprise apps, including mobile apps</li>
-      </ul>
+        <p>ReactsFacts</p>
       </header>
-      
+      <main className="App-main">
+        <h1>Fun facts about React</h1>
+        <ul className="App-list">
+          <li>Was first released in 2013</li>
+          <li>Was originally created by Jordan Walke</li>
+          <li>Has well over 100K stars on GitHub</li>
+          <li>Is maintained by Facebook</li>
+          <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+      </main>
     </div>
   );
 }
